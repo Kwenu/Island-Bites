02fb2487faca77e0eaae9c0a6c27ee6c9b5a649c
HasinduRD <hasindu.20220232@iit.ac.lk>
2024-02-20 12:18:05 +0530

    initial commit
    Added the profile page

--- src/Pages/Profilepage/profile.js
+++ src/Pages/Profilepage/profile.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import './profile.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faGear, faHeart, faPlane, faSignOut, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLaptop } from "@fortawesome/free-solid-svg-icons";
+import Profile from '../../images/profilePic.jpg';
 
-function profile() {
+function App() {
   return (
-    <div><h1>profile</h1></div>
-  )
+    <div className="profile">
+      <div className='images'>
+        <img src='https://images.pexels.com/photos/10837800/pexels-photo-10837800.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' className='cover'></img>
+        <img src= {Profile} alt='' className='profilePic'></img>
+      </div>
+      <div className='ProfileContainer'>
+        <div className='uInfo'>
+          <div className='left'>
+          </div>
+          <div className='centerp'>
+            <span>Adrian Brewer</span>
+            <div className='info'>
+              <div className='item'>
+                <span>adrianbrewer@gmail.com</span>
+              </div>
+            </div>
+          </div>
+          <div className='right'></div>
+        </div>
+
+        <div className='bar'>
+          <div className='center'>
+            <FontAwesomeIcon icon={faUser} />
+            <span>Edit Profile</span>
+          </div>
+        </div>
+
+        <div className='bar'>
+          <div className='center'>
+            <FontAwesomeIcon icon={faHeart} />
+            <span>Favourites</span>
+          </div>
+        </div>
+
+        <div className='bar'>
+          <div className='center'>
+            <FontAwesomeIcon icon={faGear} />
+            <span>Settings</span>
+          </div>
+        </div>
+
+        <div className='bar'>
+          <div className='center'>
+            <FontAwesomeIcon icon={faSignOut} />
+            <span>Log out</span>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
-export default profile
+export default App;
