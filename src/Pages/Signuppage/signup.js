d95fd060f7d67dc98ff8aa040fff7e1f627e916e
Maheli2004 <maheli.20211110@iit.ac.lk>
2024-03-17 08:40:01 +0530

    Update the authentication functionalities on the signup page and logout page

--- src/Pages/Signuppage/signup.js
+++ src/Pages/Signuppage/signup.js
@@ -1,12 +1,41 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link
+import { Link, useNavigate } from 'react-router-dom'; // Import Link
 import IMAGE from '../../images/img.jpg';
 import LOGO from '../../images/logo.jpg';
 import '../../Pages/Signuppage/signup.css';
 import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import eye icons
+import { IoMdAdd } from 'react-icons/io';
+import axios from "axios";
 
 const Signup = () => {
+
+    const [values,setValues] = useState({
+        username: "",
+        email: "",
+        name: "",
+        contactNum: "",
+        country: "",
+        password: ""
+    })
+
+    const navigate = useNavigate()
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post("http://localhost:8800/signup" ,values)
+        .then(res => {
+            if(res.data.status === "Success") {
+                navigate("/login")
+
+            } else{
+                alert("Error");
+            }
+        })
+        .catch(err => console.log(err));
+
+    }
+
     const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+
 
     return (
     <div className="w-full h-screen flex" id='container'>
@@ -20,31 +49,52 @@ const Signup = () => {
         <h3 className='text-2x1 font-bold mb-4' id='login'>Sign up</h3>
         {/* <p className='text-sm mb-2' id='welcome'>Welcome back ! Please enter your details</p> */}
     </div>
-
     <div className='w-full flex flex-col' id='detail'>
-        <p className='firstName'>First Name</p>
+        <p className='username'>Username</p>
         <input 
             type="text" 
-            placeholder='first name'
-            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' id='firstName'/>
+            placeholder='username'
+            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' 
+            id='username' 
+            name='username'
+            onChange={e => setValues({...values,username: e.target.value})}/>
         
-        <p className='lastName'>lastName</p>
+        <p className='name'>Name</p>
         <input 
             type="text" 
-            placeholder='lastName'
-            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' id='lastName'/>
+            placeholder='name'
+            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' 
+            id='name' 
+            name='name'
+            onChange={e => setValues({...values,name: e.target.value})}/>
         
+
         <p className='contactNum'>Contact Number</p>
         <input 
             type="number" 
             placeholder='contactNum'
-            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' id='contactNum'/>
+            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' 
+            id='contactNum' 
+            name='contactNum' 
+            onChange={e => setValues({...values,contactNum: e.target.value})}/>
+        
+        <p className='country'>country</p>
+        <input 
+            type="text" 
+            placeholder='country'
+            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' 
+            id='country' 
+            name='country' 
+            onChange={e => setValues({...values,country: e.target.value})}/>
         
         <p className='email'>E-mail</p>
         <input 
             type="email" 
             placeholder='example@gmail.com'
-            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' id='email'/>
+            className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none' 
+            id='email' 
+            name='email' 
+            onChange={e => setValues({...values,email: e.target.value})}/>
         
         <div className="relative">
             <p className='password'>Password</p>
@@ -52,7 +102,11 @@ const Signup = () => {
                 <input 
                     type={showPassword ? "text" : "password"} 
                     placeholder='@#*%'
-                    className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none pr-10' id='password'/>
+                    className='w-full text-black py-4 my-1 bg-transparent outline-none focus:outline-none pr-10' 
+                    id='password' 
+                    name='password' 
+                    onChange={e => setValues({...values,password: e.target.value})}/>
+
                 <div className="h-full border-l border-gray-400"></div> {/* Vertical line */}
                 <span 
                     className="eye-icon px-3"
@@ -65,14 +119,15 @@ const Signup = () => {
 
 
     <div className='w-full flex flex-col' id='signin'>
-    <Link to="/Login">
-        <button className='w-full font-bold text-white bg-[#65B741] rounded-2xl p-4 text-center flex items-center justify-center' id='button'>
-            Sign up
-        </button>
-    </Link>
+        
+    <button
+    className='w-full font-bold text-white bg-[#65B741] rounded-2xl p-4 text-center flex items-center justify-center'
+    id='button'
+    onClick={handleSubmit}>
+    Sign up
+    </button>
+
     </div>
-
-
 </div>
 
 </div>
