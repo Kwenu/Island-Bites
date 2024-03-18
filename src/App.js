5825750f18b4f0272416d6ead5eda2aad404cdf9
EbanSerug <eban.20220586@iit.ac.lk>
2024-03-18 14:13:51 +0530

    share commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import Footer from './components/footer.js'
 import ImageUpload from './pages/UplaodImage/Image.js'
 // import AddRecipe from './pages/AddRecipe/Upload.js';
+import recipe from './pages/Recipepage/recipe.js';
 
 function App() {
   return (
