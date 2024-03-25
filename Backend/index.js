1e256b4666f1df92d865d3f5d3d52285ec159483
EbanSerug <eban.20220586@iit.ac.lk>
2024-03-25 08:55:48 +0530

    Bcakend Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,2 +1,18 @@
- 
+app.post("/recipes", upload.single('file'), (req,res)=>{
+    const q = "INSERT INTO recipes (ingredients,description,instructions,img,userId,createdAt) VALUES (?)";
+    const values = [         
+        req.body.ingredients,
+        req.body.description,
+        req.body.instructions,
+        req.file.filename,
+        req.body.userId,
+        req.body.createdAt,
+];
+
+    db.query(q,[values], (err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Recipe has been created successfully..");
+    });
+
+});
 
--- src/Pages/Addrecipe/add.js
+++ src/Pages/Addrecipe/add.js
@@ -1,44 +1,84 @@
-import React from 'react';
-import '../Addrecipe/add.css';
+import React, { useState } from 'react'
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import "../Addrecipe/add.css";
 
-function Add() {
+const Add = () => {
+
+
+  const navigate = useNavigate();
+
+  const [description,setDescription] = useState("");
+  const [ingredients,setIngredients] = useState("");
+  const [instructions,setInstructions] = useState("");
+  const [time,setTime] = useState("");
+  const [userId,setUserId] = useState("");
+  const[file,setFile] = useState("");
+
+  const upload = () =>{
+    const formData = new FormData()
+    formData.append("description", description);
+    formData.append("ingredients", ingredients);
+    formData.append("instructions", instructions);
+    formData.append("time", time);
+    formData.append("userId", userId);
+    formData.append("file", file)
+
+    axios.post('http://localhost:8800/recipes',formData)
+    .then((response) => {
+      console.log(response);
+      navigate("/");
+    })
+    .catch(er => console.log(er))
+  }
+
   return (
-    <div className="add">
-      <div>
-        <label className='Label' htmlFor="description">Description.</label>
-        <textarea id="description"></textarea>
+    <div className="add-container">
+      <h2 className='add-h2'>Add Your Recipe</h2>
+      <h3 className="add-h3">  &nbsp;  &nbsp;"Share your culinary creations with the world! Your recipe will inspire others to cook delicious meals and create lasting memories.<br/> &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Let's build a vibrant community cookbook together!"</h3>
+
+      <div className="form-group">
+        <label htmlFor="description">Recipe Title : </label>
+        <textarea id="description"name='description' 
+        onChange={(e) => setDescription(e.target.value)}></textarea>
       </div>
 
-      <div>
-        <label className='Label' htmlFor="ingredients">Ingredients.</label>
-        <textarea id="ingredients"></textarea>
+      <div className="form-group">
+        <label htmlFor="ingredients">Recipe Ingredients : </label>
+        <textarea id="ingredients" name='ingredients' 
+        onChange={(e) => setIngredients(e.target.value)}></textarea>
       </div>
 
-      <div>
-        <label className='Label' htmlFor="instructions">Recipe Instructions.</label>
-        <textarea id="instructions"></textarea>
+      <div className="form-group">
+        <label htmlFor="instructions">Recipe Instructions : </label>
+        <textarea id="instructions" name='instructions' 
+        onChange={(e) => setInstructions(e.target.value)} ></textarea>
       </div>
 
-      <div>
-        <label className='Label' htmlFor="time">Time.</label>
-        <input type="text" id="time" />
+      <div className="form-group">
+        <label htmlFor="time">Cooking Duration (In min) : </label>
+        <input type="text" id="time" name='time' 
+        onChange={(e) => setTime(e.target.value)}></input>
       </div>
 
-      <div>
-        <h3>Upload Picture (Optional).</h3>
-        <div className="image-container">
-            <input type="file" id="image" accept="image/*" />
-            <label htmlFor="image" className="choose-file-button">
-                Choose File
-            </label>
-       </div>
-     </div>
-     <hr />
-      <div>
-        <button type="submit">Submit</button>
+      <div className="form-group">
+        <label htmlFor="userId">User ID :</label>
+        <input type="text" id="userId" name='userId' 
+        onChange={(e) => setUserId(e.target.value)}></input>
       </div>
+
+      <div className="form-group">
+        <div className="Add-image-container">
+        <label className='form-label'>Upload Picture (Optional)</label>
+        <input className="form-control form-control-lg" type='file' onChange={(e) => setFile (e.target.files[0])}/>
+        </div>
+      </div>
+
+      <button type='button' className='add-button' onClick={upload}>Submit</button>
+
     </div>
-  );
+  )
 }
 
 export default Add;
