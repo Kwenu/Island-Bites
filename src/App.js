f4cba4516fd320829bfe725307811ba41f33f418
Maheli2004 <maheli.20211110@iit.ac.lk>
2024-03-25 08:31:58 +0530

    Add update.js and update.css

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Pages/Loginpage/login.js';
 import Signup from './Pages/Signuppage/signup.js';
 import Sidebar from './components/Sidebar/Sidebar.js';
-import Home from './Pages/Homepage/home.js';
 import Favorite from './Pages/Favoritepage/favorite.js';
 import Profile from './Pages/Profilepage/profile.js';
+import ForgotPassword from './Pages/ForgotPasswordPage/ForgotPassword.js';
+import ResetPassword from './Pages/ForgotPasswordPage/ResetPassword.js';
 import RecipeDetails from './Pages/Recipepage/RecipeDetails.js';
 import Upload from './Pages/Uploadpage/upload.js';
 import Logout from './Pages/Logoutpage/logout.js';
 import Edit from './Pages/Editprofilepage/edit.js';
-import Add from './Pages/Addrecipe/add.js';
-import Forgot from './Pages/ForgotPassword/Forgot.js';
-import Verify from './Pages/ForgotPassword/Verify.js';
-import New from './Pages/ForgotPassword/New.js';
-import Recipe from './Pages/Recipepage/recipe.js';
-
-import Recipes from './Pages/Recipes.jsx';
-import Addd from './Pages/Addd.jsx';
-import Update from './Pages/Update.jsx';
-import Cards from './Pages/Cards.jsx';
+import Add from './Pages/Addrecipe/Add.jsx';
+import Cards from './Pages/RecipeCardspage/Cards.jsx';
+import Recipes from './Pages/Recipepage/Recipes.jsx';
 
 import Myrecipe from './Pages/Profilepage/myrecipe.js';
+import Update from './Pages/Profilepage/update.js';
 
 const App = () => {
   return (
@@ -31,9 +26,8 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup/>}/>
-        <Route path="/forgot" element={<Forgot/>} />
-        <Route path="/verify" element={<Verify/>} />
-        <Route path="/new" element={<New/>} />
+        <Route path="/forgot" element={<ForgotPassword/>} />
+        <Route path="/reset" element={<ResetPassword/>} />
       </Routes>
 
 
@@ -41,12 +35,9 @@ const App = () => {
       <Sidebar>
         <Routes>
           <Route path="/" element={<Cards />} />
-          <Route path="/addd" element={<Addd />} />
+          <Route path="/add" element={<Add />} />
           <Route path="/recipes" element={<Recipes />} />
-          <Route path="/recipe" element={<Recipe />} />
-          <Route path="/update" element={<Update />} />
           <Route path="/cards" element={<Cards />} />
-          <Route path="/Home" element={<Home />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
           <Route path="/favorite" element={<Favorite />} />
           <Route path="/upload" element={<Upload />} />
